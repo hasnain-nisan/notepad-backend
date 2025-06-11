3f2076952a436792d25ebaa8e4e524dcb1b8e9f8
Hasnain Ahamed Nisan <68366713+hasnain-nisan@users.noreply.github.com>
2025-06-11 23:48:31 +0600

    initial commit

---
+++ src/common/interfaces/repository.interface.ts
@@ -0,0 +1,7 @@
+export interface IRepository<T> {
+  findAll(): Promise<T[]>;
+  findById(id: string): Promise<T | null>;
+  create(entity: Partial<T>): Promise<T>;
+  update(id: string, entity: Partial<T>): Promise<T | null>;
+  delete(id: string): Promise<boolean>;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+// import { Note } from '../../notes/entities/note.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  //   @OneToMany(() => Note, (note) => note.user)
+  //   notes: Note[];
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+}
